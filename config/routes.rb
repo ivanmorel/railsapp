da73af966d9210185863f70fdb4840637616626b
Ivan Morel <ivanmorel94@gmail.com>
2017-06-03 10:47:04 -0400

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :article
+  get 'people/test', to: 'people#test'
   resources :people
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
